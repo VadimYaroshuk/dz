64266ca48107fb0ec32aea33864d560ed97a5119
Vadim Yaroshuk <vadim@yaroshuk.spb.ru>
2022-11-03 17:17:41 +0300

    Доделал 2 дз

---
+++ 05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int ostatok = 0;
+int result = 0;
+
+ostatok = N % 100;
+result = ostatok / 10;
+
+Console.Write("Вторая цифра числа: ");
+Console.WriteLine(result);
+
---
+++ 06/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Введите трехзначное число: ");
+var N = Convert.ToInt32(Console.ReadLine());
+var ostatok = 0;
+
+ostatok = N / 100;
+
+if (ostatok >= 1)
+
+{
+    if (ostatok <= 10)
+    {
+        Console.Write(N / 100);
+
+    }
+    else
+    {
+        Console.Write(ostatok % 10);
+    }
+}
+else
+{
+    Console.Write("3ей цифры числа нет");
+
+}
+
+
